c6639870aebeba29c481aac680b86df7387f2107
Ivan Pizarroso <ivnp31@gmail.com>
2023-06-03 20:54:42 +0200

    peliculas enganchada

--- src/app/modules/front-pages/pages/films/films-routing.module.ts
+++ src/app/modules/front-pages/pages/films/films-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmsListComponent } from './films-list/films-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FilmsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
