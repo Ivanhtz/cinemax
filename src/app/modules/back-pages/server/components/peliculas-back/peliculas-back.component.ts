f67bda57a862eb819fb43ee1e7111467502b45c0
Janto7 <selicoslive@gmail>
2023-06-03 11:20:55 +0200

    Creación tablas en estructura back-end

--- src/app/modules/back-pages/server/components/peliculas-back/peliculas-back.component.ts
+++ src/app/modules/back-pages/server/components/peliculas-back/peliculas-back.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Pelicula {
+  title: string;
+  director: string;
+  releaseYear: number;
+}
+
+const ELEMENT_DATA: Pelicula[] = [
+  {title: 'Toy Story', director: 'John Lasseter', releaseYear: 1995},
+  // more movies...
+];
+
 @Component({
   selector: 'app-peliculas-back',
   templateUrl: './peliculas-back.component.html',
   styleUrls: ['./peliculas-back.component.scss']
 })
 export class PeliculasBackComponent {
-
+  displayedColumns: string[] = ['title', 'director', 'releaseYear'];
+  dataSource = ELEMENT_DATA;
 }
