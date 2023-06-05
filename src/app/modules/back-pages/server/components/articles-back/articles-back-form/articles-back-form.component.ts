15ff651a7640b84c7908051af6ae794d8fca2fab
Janto7 <selicoslive@gmail>
2023-06-05 17:54:10 +0200

    Estructura back Articles

--- src/app/modules/back-pages/server/components/articles-back/articles-back-form/articles-back-form.component.ts
+++ src/app/modules/back-pages/server/components/articles-back/articles-back-form/articles-back-form.component.ts
@@ -1,94 +1,78 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Iarticle } from 'src/app/interfaces/iarticle.interface';
 
-import { FormBuilder, Validators } from '@angular/forms';
-
+import { ArticlesService } from 'src/app/services/articles-service/articles.service';
 
 @Component({
   selector: 'app-articles-back-form',
   templateUrl: './articles-back-form.component.html',
-  styleUrls: ['./articles-back-form.component.scss']
+  styleUrls: ['./articles-back-form.component.scss'],
 })
-export class ArticlesBackFormComponent {
-  private fb = inject(FormBuilder);
-  addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [null, Validators.compose([
-      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    ],
-    shipping: ['free', Validators.required]
-  });
+export class ArticlesBackFormComponent implements OnInit {
+  articleForm: FormGroup;
+  editingArticle: Iarticle;
+  isEditing: boolean = false;
 
-  hasUnitNumber = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private articlesService: ArticlesService
+  ) {
+    this.articleForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      image: ['', Validators.required],
+      content: ['', Validators.required]
+    });
+    this.editingArticle = {
+      id: 0,
+      title: '',
+      image: '',
+      content: ''
+    };
+  }
 
-  states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
-  ];
+  ngOnInit(): void {
+    this.articlesService.articleBeingEdited$.subscribe((article) => {
+      if (article) {
+        this.isEditing = true;
+        this.editingArticle = article;
+        this.articleForm = this.formBuilder.group({
+          title: [article.title, Validators.required],
+          image: [article.image, Validators.required],
+          content: [article.content, Validators.required]
+        });
+      } else {
+        this.isEditing = false;
+        this.editingArticle = {
+          id: 0,
+          title: '',
+          image: '',
+          content: ''
+        };
+        this.articleForm = this.formBuilder.group({
+          title: ['', Validators.required],
+          image: ['', Validators.required],
+          content: ['', Validators.required]
+        });
+      }
+    });
+  }
 
   onSubmit(): void {
-    alert('Thanks!');
+    if (this.isEditing) {
+      this.articlesService
+        .updateArticle(this.editingArticle.id, this.articleForm.value)
+        .subscribe(() => {
+          this.articlesService.stopEditingArticle();
+        });
+    } else {
+      this.articlesService.createArticle(this.articleForm.value).subscribe(() => {
+        this.articlesService.stopEditingArticle();
+      });
+    }
+  }
+
+  cancel(): void {
+    this.articlesService.stopEditingArticle();
   }
 }
