eb50054824b0632ea141992575d9b01ddfce3134
pabloTorrecilla <ptorrecillarodriguez@gmail.com>
2023-06-07 11:47:51 +0200

    estructura-users-register

--- src/app/modules/front-pages/pages/users-register/users-register.module.ts
+++ src/app/modules/front-pages/pages/users-register/users-register.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRegisterRoutingModule } from './users-register-routing.module';
+import { UserComponent } from './user/user.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserComponent,
+    UsersListComponent,
+    ViewUserComponent
+  ],
   imports: [
     CommonModule,
     UsersRegisterRoutingModule
